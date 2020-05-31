3a0058fc2251c1bd766c7c299281d3b0cdbeba69
Matthias Kolley <matthias.kolley@googlemail.com>
2020-05-31 21:24:45 +0200

    Fix game crash on player death.

--- 3_Project_Boost/Assets/Scripts/Rocket.cs
+++ 3_Project_Boost/Assets/Scripts/Rocket.cs
@@ -14,13 +14,13 @@ public class Rocket : MonoBehaviour
     [SerializeField] float mainThrust = 1.0f;
     [SerializeField] float levelTransitionWaitTime = 3.0f;
     [SerializeField] AudioClip mainEngine = null;
-    //[SerializeField] AudioClip death = null; // handled by Destroyable Player
+    [SerializeField] AudioClip death = null; // handled by Destroyable Player
     [SerializeField] AudioClip success = null;
     [SerializeField] ParticleSystem[] mainEngineParticles = null;
-    //[SerializeField] ParticleSystem deathParticles = null; // handled by Destroyable Player
+    [SerializeField] ParticleSystem deathParticles = null; // handled by Destroyable Player
     [SerializeField] ParticleSystem successParticles = null;
 
-    [SerializeField] GameObject destroyedVersion = null;
+    // [SerializeField] GameObject destroyedVersion = null;
     bool collisionEnabled = true;
     // Start is called before the first frame update
     void Start()
@@ -90,20 +90,22 @@ public class Rocket : MonoBehaviour
     private void KillPlayer()
     {
         print("Dead");
-        // this.state = State.Dying;
-        // this.rigidBody.constraints = RigidbodyConstraints.None;
-        // this.audioSource.Stop();
-        // this.audioSource.PlayOneShot(this.death); //This is played by destroyed object.
+        this.state = State.Dying;
+        this.rigidBody.constraints = RigidbodyConstraints.None;
+        this.audioSource.Stop();
+        
         this.ExplodePlayer();
         Invoke("ResetLevel", levelTransitionWaitTime);
     }
 
     private void ExplodePlayer()
     {
-        var gameObject = Instantiate(this.destroyedVersion, transform.position, transform.rotation);
-        var rocket = gameObject.GetComponent<Rocket>();
-        rocket.collisionEnabled = this.collisionEnabled;
-        gameObject.SetActive(false);
+        this.audioSource.PlayOneShot(this.death);
+        this.deathParticles.Play();
+        // var gameObject = Instantiate(this.destroyedVersion, transform.position, transform.rotation);
+        //var rocket = gameObject.GetComponent<Rocket>();
+        //rocket.collisionEnabled = this.collisionEnabled;
+        // this.gameObject.SetActive(false);
     }
 
     private void Transcend()

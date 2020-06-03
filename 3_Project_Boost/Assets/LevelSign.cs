058f9e3b557345e57562912778d5b93920c53c5c
Matthias Kolley <matthias.kolley@googlemail.com>
2020-06-03 23:35:05 +0200

    Add Info Signs for level name.

---
+++ 3_Project_Boost/Assets/LevelSign.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSign : MonoBehaviour
+{
+    TextMeshPro textMesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.textMesh = GetComponentInChildren<TextMeshPro>();
+        this.textMesh.text = "Level " + (SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

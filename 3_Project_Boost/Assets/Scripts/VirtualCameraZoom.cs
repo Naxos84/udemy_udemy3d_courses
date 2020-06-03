52b0ac74e096e5bd39bd0e722c4c8d46682625e0
Matthias Kolley <matthias.kolley@googlemail.com>
2020-06-03 23:03:25 +0200

    Add Zoom to MouseWheel and add Controls description to first levels.

---
+++ 3_Project_Boost/Assets/Scripts/VirtualCameraZoom.cs
@@ -0,0 +1,24 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualCameraZoom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    CinemachineVirtualCamera virtualCamera;
+    void Start()
+    {
+        this.virtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CinemachineTransposer transposer = this.virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        Vector3 currentOffset = transposer.m_FollowOffset;
+        float newOffset = currentOffset.z + Input.mouseScrollDelta.y;
+        print("New Offset " + newOffset);
+        transposer.m_FollowOffset = new Vector3(currentOffset.x, currentOffset.y, Mathf.Clamp(currentOffset.z + Input.mouseScrollDelta.y, -50f, -5f));
+    }
+}

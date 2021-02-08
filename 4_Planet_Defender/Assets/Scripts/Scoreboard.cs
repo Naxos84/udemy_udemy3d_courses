771785db0bb2c2bb9af5b9877659519e1b97c023
Matthias Kolley <matthias.kolley@googlemail.com>
2021-02-09 00:28:24 +0100

    Lecture 106 Add Scoreboard

---
+++ 4_Planet_Defender/Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Scoreboard : MonoBehaviour
+{
+
+    [SerializeField] int scorePerHit = 12;
+    int currentScore = 0;
+    Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.scoreText = GetComponent<Text>();
+        this.scoreText.text = currentScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ScoreHit()
+    {
+        this.currentScore += this.scorePerHit;
+        this.scoreText.text = currentScore.ToString();
+    }
+
+}
